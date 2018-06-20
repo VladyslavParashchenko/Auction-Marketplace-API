395e4c7d93e371bc6cf747661da87d578cd6098c
Vladyslav Paraschenko <mrfenix2015@gmail.com>
2018-06-20 16:26:57 +0300

    add test for connection to action cable

---
+++ spec/channels/bid_connection_to_cable_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+require "action_cable/testing/rspec"
+require "action_cable/testing/rspec/features"
+RSpec.describe ApplicationCable::Connection, type: :channel do
+  before(:each) do
+    @user = create(:client)
+    @lot = create(:lot, user: @user)
+  end
+  it "successfully connects" do
+    connect "/lots/#{@lot.id}/", headers: @user.create_new_auth_token
+    expect(connection.current_user.id).to eq(@user.id)
+  end
+  it "rejects connection" do
+    expect { connect "/cable" }.to have_rejected_connection
+  end
+end
