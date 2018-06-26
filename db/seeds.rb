a4d837b9b37dc21c206dacdc00b4cb54c7b46f25
Vladyslav Paraschenko <mrfenix2015@gmail.com>
2018-06-26 13:46:33 +0300

    add seeds and delete config

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,13 @@ users.each { |user| FactoryBot.create_list(:lot, 5, user: user) }
 Lot.all.each do |lot|
   users.each do |user|
     if user.id != lot.user.id
-      create(:bid, user: user, lot: lot)
+      FactoryBot.create(:bid, user: user, lot: lot)
     end
+  end
+end
+Lot.all.each do |lot|
+  if lot.id.even?
+    bid = FactoryBot.create(:bid,proposed_price: lot.estimated_price+100,lot: lot)
+    FactoryBot.create(:order,bid: bid)
   end
 end
